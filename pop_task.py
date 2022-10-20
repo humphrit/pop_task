6a3d4a7eaa49833789e2e69f3291a296bb5416f5
tomhumphries-starstock <tomhumphries@starstock.com>
2022-10-20 21:53:20 +0100

    chore: tidying up

--- pop_task.py
+++ pop_task.py
@@ -1,26 +1,18 @@
-import os
-import time
-import copy
-import plotly
+from typing import List
+
 import numpy as np
-import pandas as pd
-
-import plotly.express as px
-
-from enum import Enum
-from typing import List, Tuple
 from plotly import graph_objects as go
 
 
-class WorldObject():
+class WorldObject:
     col = None
 
-    def __init__(self, obj_id, initx, inity):
+    def __init__(self, obj_id: int, initx: int, inity: int):
         self.obj_id = obj_id
         self.posx = initx
         self.posy = inity
 
-    def move(self, step_x, step_y):
+    def move(self, step_x: int, step_y: int):
         self.posx += step_x
         self.posy += step_y
 
@@ -32,10 +24,13 @@ class WorldObject():
 
 
 class Bubble(WorldObject):
-    col = "green"
-    state = "floating"
 
-    def __init__(self, obj_id, initx, inity):
+    state = "floating"
+    col = "green"
+
+    def __init__(self, obj_id: int, initx: int, inity: int):
+        self.col = "green"
+        self.state = "floating"
         super().__init__(obj_id, initx, inity)
 
     def move_one_point_in_random_direction(self):
@@ -56,7 +51,9 @@ class Bubble(WorldObject):
             self.col = "blue"
 
     def draw(self):
-        return go.Scatter(x=[self.posx], y=[self.posy], mode="markers", marker={"color": self.col})
+        return go.Scatter(
+            x=[self.posx], y=[self.posy], mode="markers", marker={"color": self.col}
+        )
 
     def burst(self):
         if self.state == "floating":
@@ -65,7 +62,7 @@ class Bubble(WorldObject):
 
 
 class Rock(WorldObject):
-    col = "yellow"
+    col = "orange"
 
     def move(self):
         pass
@@ -74,35 +71,50 @@ class Rock(WorldObject):
         pass
 
     def draw(self):
-        return go.Scatter(x=[self.posx], y=[self.posy], mode="markers", marker={"color": self.col})
+        return go.Scatter(
+            x=[self.posx], y=[self.posy], mode="markers", marker={"color": self.col}
+        )
 
 
-class WorldGrid():
+class WorldGrid:
     def __init__(self, width: int, height: int, w_objects: List[WorldObject]):
         self.width = width
         self.height = height
         self.w_objects = w_objects
-        self.rock_positions = [x.position() for x in self.w_objects if isinstance(x, Rock)]
+        self.rock_positions = [
+            x.position() for x in self.w_objects if isinstance(x, Rock)
+        ]
+
+    def object_positions(self):
+        return [
+            x.position()
+            for x in self.w_objects
+            if isinstance(x, Bubble) and x.state == "floating"
+        ] + self.rock_positions
 
     def update_state(self):
         for w_object in self.w_objects:
             w_object.update_state()
-
+        self.burst_bubbles()
 
     def burst_bubbles(self):
         positions = self.object_positions()
         seen = set()
         duplicate_positions = [x for x in positions if x in seen or seen.add(x)]
         for w_object in self.w_objects:
-            if w_object.position() in duplicate_positions and isinstance(w_object, Bubble):
+            if w_object.position() in duplicate_positions and isinstance(
+                w_object, Bubble
+            ):
                 w_object.burst()
 
     def get_frame(self, frame_number: int):
         draw_objects = []
         for w_object in self.w_objects:
             draw_objects.append(w_object.draw())
-        return go.Frame(data=draw_objects,
-                        layout=go.Layout(title_text=f"Simulation frame {frame_number}"))
+        return go.Frame(
+            data=draw_objects,
+            layout=go.Layout(title_text=f"Simulation frame {frame_number}"),
+        )
 
     def create_simulation(self, steps: int):
         frames = []
@@ -110,28 +122,39 @@ class WorldGrid():
             self.update_state()
             frames.append(self.get_frame(i))
 
-        fig = go.Figure(data=[w_obj.draw() for w_obj in self.w_objects],
-            layout=go.Layout(xaxis=dict(range=[0, self.width], autorange=False),
-                yaxis=dict(range=[0, self.height], autorange=False), title="Simulation",
-                updatemenus=[dict(type="buttons",
-                    buttons=[dict(label="Play", method="animate", args=[None])])]), frames=frames)
+        fig = go.Figure(
+            data=[w_obj.draw() for w_obj in self.w_objects],
+            layout=go.Layout(
+                xaxis=dict(range=[0, self.width], autorange=False),
+                yaxis=dict(range=[0, self.height], autorange=False),
+                title="Simulation",
+                updatemenus=[
+                    dict(
+                        type="buttons",
+                        buttons=[dict(label="Play", method="animate", args=[None])],
+                    )
+                ],
+            ),
+            frames=frames,
+        )
         fig.show()
-
-    def object_positions(self):
-        return [x.position() for x in self.w_objects if isinstance(x, Bubble) and x.state ==
-                "floating"] + self.rock_positions
-
 
 
 def simulate():
-    bubbles = [Bubble(f"bubble_{i}_{j}", i, j) for i in range(5, 15, 2) for j in range(5, 15, 2)]
-    rocks = [Rock("rock_2_2", 2, 2), Rock("rock_18_18", 18, 18), Rock("rock_3_15", 3, 15),
-
+    bubbles = [
+        Bubble(f"bubble_{i}_{j}", i, j)
+        for i in range(5, 15, 2)
+        for j in range(5, 15, 2)
+    ]
+    rocks = [
+        Rock("rock_2_2", 2, 2),
+        Rock("rock_18_18", 18, 18),
+        Rock("rock_3_15", 3, 15),
     ]
     world = WorldGrid(20, 20, bubbles + rocks)
     world.create_simulation(steps=50)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
 
     simulate()
-
--- test_pop_task.py
+++ test_pop_task.py
@@ -1,23 +1,28 @@
+from unittest import mock
 import pytest
 
-from pop_task import Bubble, Rock, WorldGrid, WorldObject
+from pop_task import Bubble, Rock, WorldGrid
 
 
 @pytest.fixture
 def floating_bubble():
     return Bubble(1, 2, 3)
 
+
 @pytest.fixture
 def another_floating_bubble():
     return Bubble(2, 3, 3)
+
 
 @pytest.fixture
 def a_third_floating_bubble():
     return Bubble(3, 3, 3)
 
+
 @pytest.fixture
 def a_fourth_floating_bubble():
     return Bubble(4, 4, 4)
+
 
 @pytest.fixture
 def burst_bubble():
@@ -26,17 +31,29 @@ def burst_bubble():
     bubble.state = "burst"
     return bubble
 
+
 @pytest.fixture
 def good_old_rock_nothing_beats_that():
     return Rock(1, 4, 4)
 
+
 @pytest.fixture
-def rock_and_bubbles(burst_bubble, floating_bubble, another_floating_bubble,
-                     a_third_floating_bubble, a_fourth_floating_bubble,
-                     good_old_rock_nothing_beats_that):
-    return [burst_bubble, floating_bubble, another_floating_bubble,
-                     a_third_floating_bubble, a_fourth_floating_bubble,
-                     good_old_rock_nothing_beats_that]
+def rock_and_bubbles(
+    burst_bubble,
+    floating_bubble,
+    another_floating_bubble,
+    a_third_floating_bubble,
+    a_fourth_floating_bubble,
+    good_old_rock_nothing_beats_that,
+):
+    return [
+        burst_bubble,
+        floating_bubble,
+        another_floating_bubble,
+        a_third_floating_bubble,
+        a_fourth_floating_bubble,
+        good_old_rock_nothing_beats_that,
+    ]
 
 
 @pytest.fixture
@@ -44,9 +61,7 @@ def world_grid(rock_and_bubbles):
     return WorldGrid(5, 5, rock_and_bubbles)
 
 
-
 class TestBubbles:
-
     def test_burst_floating_bubble(self, floating_bubble):
         floating_bubble.burst()
         assert floating_bubble.state == "burst"
@@ -55,7 +70,7 @@ class TestBubbles:
     def test_burst_burst_bubble(self, burst_bubble):
         burst_bubble.burst()
         assert burst_bubble.state == "burst"
-        assert burst_bubble.col == 'blue'
+        assert burst_bubble.col == "blue"
 
     def test_update_just_burst_bubble(self, floating_bubble):
         floating_bubble.burst()
@@ -65,24 +80,75 @@ class TestBubbles:
         assert floating_bubble.state == "burst"
         assert floating_bubble.col == "blue"
 
-    def test_move_one_point_in_random_direction(self, floating_bubble):
-        original_x, original_y = floating_bubble.position()
+    # TODO: test and code to ensure bubble doesn't move off plot or bursts at edge
+    @pytest.mark.parametrize(
+        "choice_return_value, new_position",
+        [
+            ("left", (1, 3)),
+            ("right", (3, 3)),
+            ("up", (2, 4)),
+            ("down", (2, 2)),
+            ("stay", (2, 3)),
+        ],
+    )
+    @mock.patch("numpy.random.choice")
+    def test_move_one_point_in_random_direction(
+        self, mocked_choice, floating_bubble, choice_return_value, new_position
+    ):
+        mocked_choice.return_value = choice_return_value
         floating_bubble.move_one_point_in_random_direction()
-        new_x, new_y = floating_bubble.position()
-        assert (abs(original_x-new_x)+abs(original_y-new_y)) == 1
+        assert floating_bubble.position() == new_position
 
 
 class TestWorldGrid:
+    # TODO: refactoring into multiple single/double assertion tests for each function
 
-    def test_burst_bubbles(self, world_grid, floating_bubble, another_floating_bubble,
-                           a_third_floating_bubble, a_fourth_floating_bubble, burst_bubble, good_old_rock_nothing_beats_that):
+    def test_burst_bubbles(
+        self,
+        world_grid,
+        floating_bubble,
+        another_floating_bubble,
+        a_third_floating_bubble,
+        a_fourth_floating_bubble,
+        burst_bubble,
+        good_old_rock_nothing_beats_that,
+    ):
         world_grid.burst_bubbles()
         assert floating_bubble.state == "floating"
         assert floating_bubble.col == "green"
         assert burst_bubble.state == "burst"
         assert burst_bubble.col == "blue"
-        for bubble in [another_floating_bubble, a_third_floating_bubble, a_fourth_floating_bubble]:
+        for bubble in [
+            another_floating_bubble,
+            a_third_floating_bubble,
+            a_fourth_floating_bubble,
+        ]:
             assert bubble.state == "burst"
             assert bubble.col == "red"
-        assert good_old_rock_nothing_beats_that.col == 'yellow'
+        assert good_old_rock_nothing_beats_that.col == "orange"
 
+    @mock.patch("pop_task.Bubble.move_one_point_in_random_direction")
+    def test_update_state(
+        self,
+        mocked_bubble_move,
+        world_grid,
+        floating_bubble,
+        another_floating_bubble,
+        a_third_floating_bubble,
+        a_fourth_floating_bubble,
+        burst_bubble,
+        good_old_rock_nothing_beats_that,
+    ):
+        world_grid.update_state()
+        assert floating_bubble.state == "floating"
+        assert floating_bubble.col == "green"
+        assert burst_bubble.state == "burst"
+        assert burst_bubble.col == "blue"
+        for bubble in [
+            another_floating_bubble,
+            a_third_floating_bubble,
+            a_fourth_floating_bubble,
+        ]:
+            assert bubble.state == "burst"
+            assert bubble.col == "red"
+        assert good_old_rock_nothing_beats_that.col == "orange"
